7b3e44943169357a1e96eaebe58949b5f3ca4acf
Nathan Trop <natetrop@gmail.com>
2019-04-24 15:17:05 -0500

    added prusa build files for future updates

--- skele_patch.py
+++ skele_patch.py
@@ -59,7 +59,7 @@ strreplace(replacefile, sfind, sreplace)
 #uncomment to lower motor current consider changing 20 to 36 for 800 mah
 sfind = '#define TMC2130_CURRENTS_H {16, 20, 35, 30}  // default holding currents for all axes'
 sreplace = '#define TMC2130_CURRENTS_H {20, 20, 35, 26}  // default holding currents for all axes'
-strreplace(replacefile,sfind,sreplace)
+#strreplace(replacefile,sfind,sreplace)
 
 sfind = '#define TMC2130_CURRENTS_R {16, 20, 35, 30}  // default running currents for all axes'
 sreplace = '#define TMC2130_CURRENTS_R {20, 20, 35, 26}  // default running currents for all axes'
