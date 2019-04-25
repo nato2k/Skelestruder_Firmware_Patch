2b5743779b9cb2b25acab8c4f3d56d9ee913411d
Nathan Trop <natetrop@gmail.com>
2019-04-24 20:28:08 -0500

    added steps to copy finished file to complete_builds directory

--- build_wsl.sh
+++ build_wsl.sh
@@ -16,10 +16,21 @@ else
         mkdir Hex-files
         echo "creating Hex-files directory to avoid Prusa build script bug"
 	else
-		mv Hex-files Hex-files-$(date +%Y%m%d)
+		mv Hex-files Hex-files-$(date +%Y%m%d%s)
 		mkdir Hex-files
     fi
 	echo "compiling firmware"
-	./Prusa-Firmware/PF-build.sh 1_75mm_MK3-EINSy10a-E3Dv6full.h EN_ONLY GOLD
+	results=$(./Prusa-Firmware/PF-build.sh 1_75mm_MK3-EINSy10a-E3Dv6full.h EN_ONLY GOLD)
+	IFS=':'; fwinfo=($results); unset IFS;
+	IFS=' '; fwdir=(${fwinfo[11]}); unset IFS;
+	echo "copying hex file to builds directory"
+	if [ ! -d complete_builds ]
+		then
+		mkdir complete_builds
+	fi
+	fwd=$(echo ${fwdir[0]} | cut -d' ' -f 1)
+	fwfile=$(ls $fwd)
+	fwfull=$fwd'/'$fwfile
+	cp $fwfull complete_builds
 	echo "complete!"
 fi
