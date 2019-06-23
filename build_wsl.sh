9aad7ad23606b50d16b127e3a2944cfd4bb214fd
7ba366163ea66f9fddd27ce0bcc442769bd301ee <7ba366163ea66f9fddd27ce0bcc442769bd301ee>
2019-06-23 10:32:50 -0500

    added arguments for esteps and usteps

--- build_wsl.sh
+++ build_wsl.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
+if [ $# < 2 ]
+  then
+    num1=980
+	num2=16
+	echo "default steps selected 980/16"
+  else
+    num1=$1
+	num2=$2
+	echo "using $num1 esteps and $num2 usteps"
+fi
+
 if [ -d Prusa-Firmware ]
 then
 	rm -rf Prusa-Firmware
 	echo "removing old directory"
 fi
 git clone https://github.com/prusa3d/Prusa-Firmware
-python3 skele_patch.py
+python3 skele_patch.py $num1 $num2
 rc=$?
 if [ $rc = 1 ]
 then
@@ -23,18 +34,15 @@ else
 	echo "compiling firmware"
 	results=$(./Prusa-Firmware/PF-build.sh 1_75mm_MK3-EINSy10a-E3Dv6full.h EN_ONLY GOLD)
     fwdir=$(echo $results | sed -n -E -e 's/.*Hex-file Folder: ([^ ]+).*/\1/p')
-	#IFS=':'; fwinfo=($results); unset IFS;
-	#IFS=' '; fwdir=(${fwinfo[11]}); unset IFS;
 	echo "copying hex file to builds directory"
 	if [ ! -d complete_builds ]
 		then
 		mkdir complete_builds
 	fi
-	#fwd=$(echo ${fwdir[0]} | cut -d' ' -f 1)
 	fwfile=$(ls $fwdir)
 	fwfull=$fwdir'/'$fwfile
 	fwnew=$(echo $fwfile | cut -d "-" -f 1-2)
-	cp $fwfull complete_builds/$fwnew-MK3-$(date +%Y%m%d).hex
+	cp $fwfull complete_builds/$fwnew-MK3-E$num1-u$num2-$(date +%Y%m%d).hex
 	echo "compiled file is located at: $(pwd)/complete_builds/$fwnew-MK3-$(date +%Y%m%d).hex"
 	echo "complete!"
 fi
--- skele_patch.py
+++ skele_patch.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 #changes to ./Prusa-Firmware/Firmware/Marlin_main.cpp
 import sys
+num1 = sys.argv[1]
+num2 = sys.argv[2]
+num1 = int(num1)
+num2 = int(num2)
+
+if num1 == 8:
+    num1 = ' 8'
 
 def strreplace(replacefile, sfind, sreplace):
     s = open(replacefile, 'r').read()
@@ -34,9 +41,8 @@ strreplace(replacefile, sfind, sreplace)
 #changes to ./Prusa-Firmware/Firmware/variants/1_75mm_MK3-EINSy10a-E3Dv6full.h comment .9 line if you have 1.8 and uncomment current line for 1.8
 replacefile = './Prusa-Firmware/Firmware/variants/1_75mm_MK3-EINSy10a-E3Dv6full.h'
 s = open(replacefile, 'r').read()
-
 sfind = '#define DEFAULT_AXIS_STEPS_PER_UNIT   {100,100,3200/8,280}'
-sreplace = '#define DEFAULT_AXIS_STEPS_PER_UNIT   {100,100,3200/8,490}'
+sreplace = '#define DEFAULT_AXIS_STEPS_PER_UNIT   {100,100,3200/8,%d}' % (num1)
 strreplace(replacefile, sfind, sreplace)
 
 sfind = '#define Z_MAX_POS 210'
@@ -53,17 +59,19 @@ strreplace(replacefile, sfind, sreplace)
 
 #.9 motor.  comment if using 1.8
 sfind = '#define TMC2130_USTEPS_E    32        // microstep resolution for E axis'
-sreplace = '#define TMC2130_USTEPS_E    8        // microstep resolution for E axis'
+sreplace = '#define TMC2130_USTEPS_E   %d        // microstep resolution for E axis' % (num2)
 strreplace(replacefile, sfind, sreplace)
 
 #uncomment to lower motor current consider changing 20 to 36 for 800 mah
 sfind = '#define TMC2130_CURRENTS_H {16, 20, 35, 30}  // default holding currents for all axes'
-sreplace = '#define TMC2130_CURRENTS_H {24, 20, 35, 30}  // default holding currents for all axes'
-strreplace(replacefile,sfind,sreplace)
+sreplace = '#define TMC2130_CURRENTS_H {20, 20, 35, 30}  // default holding currents for all axes'
+if num2 < 16:
+    strreplace(replacefile,sfind,sreplace)
 
 sfind = '#define TMC2130_CURRENTS_R {16, 20, 35, 30}  // default running currents for all axes'
-sreplace = '#define TMC2130_CURRENTS_R {24, 20, 35, 30}  // default running currents for all axes'
-strreplace(replacefile,sfind,sreplace)
+sreplace = '#define TMC2130_CURRENTS_R {20, 20, 35, 30}  // default running currents for all axes'
+if num2 < 16:
+    strreplace(replacefile,sfind,sreplace)
 
 #disable forced selftest in ./Prusa-Firmware/Firmware/variants/1_75mm_MK3-EINSy10a-E3Dv6full.h
 f = open('./Prusa-Firmware/Firmware/variants/1_75mm_MK3-EINSy10a-E3Dv6full.h', 'a+')
